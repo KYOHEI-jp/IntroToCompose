5917a7afc6e599d9968db911baacbc2fb885de4a
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-02-05 11:14:54 +0900

    @Previewと@Composable使って年齢表示

--- app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.introtocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
 
@@ -22,12 +25,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        showAge()
+                        Greeting("Android")
+                    }
                 }
             }
         }
     }
 }
+
+@Preview
+@Composable
+fun showAge(age: Int = 12) {
+    Text(text = age.toString())
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
